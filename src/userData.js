567835535e68dcefb7159e78e2b87bee11980e7f
ananthujp <31985570+ananthujp@users.noreply.github.com>
2024-03-06 21:12:08 +0530

    Fixes

---
+++ src/userData.js
@@ -0,0 +1,6 @@
+export const emails = [
+  { e: "rupamm.pe.ug@jadavpuruniversity.in", pw: "rupanm%@colab.2024" },
+  { e: "kavya.gotecha@iitgn.ac.in", pw: "kavya!#colab.2024" },
+  { e: "shrushtithakkar.02@hotmail.com", pw: "thakkar^@colab.2024" },
+  { e: "sravanthidulapalli@iitgn.ac.in", pw: "sravanthi!^colab.2024" },
+];
